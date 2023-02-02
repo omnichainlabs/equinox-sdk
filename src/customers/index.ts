f8f47bc372436f9a5909d9f15c42666df9cf97b6
Eugene Y. Q. Shen <eyqs@dydx.exchange>
2023-02-02 15:33:46 -0800

    add backend queries for /customers

---
+++ src/customers/index.ts
@@ -0,0 +1,83 @@
+import { parseFetchResponse } from '../index.js'
+import {
+  Customer,
+  Email,
+  UserProps
+} from '../types.js'
+
+export async function getAllCustomers (user: UserProps, projectId: string): Promise<Customer[]> {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BEANSTALK_SERVER_URL}/customers/${projectId}`, {
+      headers: {
+        'x-api-key': user.apikey,
+        'x-user-id': user.email
+      }
+    })
+    return await parseFetchResponse(response)
+  } catch (err) {
+    console.error(`Error occurred when trying to GET /customers/${projectId}`)
+    console.error(err)
+  }
+  return []
+}
+
+export async function getCustomer ({
+  user,
+  projectId,
+  customerId
+}: {
+  user: UserProps
+  projectId: string
+  customerId: Email
+}): Promise<Customer | undefined> {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BEANSTALK_SERVER_URL}/customers/${projectId}/${customerId}`, {
+      headers: {
+        'x-api-key': user.apikey,
+        'x-user-id': user.email
+      }
+    })
+    return await parseFetchResponse(response)
+  } catch (err) {
+    console.error(`Error occurred when trying to GET /customers/${projectId}/${customerId}`)
+    console.error(err)
+  }
+}
+
+export async function deleteCustomer (user: UserProps, customer: Customer): Promise<void> {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BEANSTALK_SERVER_URL}/customers`, {
+      method: 'DELETE',
+      mode: 'cors',
+      body: JSON.stringify(customer),
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': user.apikey,
+        'x-user-id': user.email
+      }
+    })
+    await parseFetchResponse(response)
+  } catch (err) {
+    console.error('Error occurred when trying to DELETE /customers')
+    console.error(err)
+  }
+}
+
+export async function postCustomer (user: UserProps, customer: Customer): Promise<Customer | undefined> {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BEANSTALK_SERVER_URL}/customers`, {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify(customer),
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': user.apikey,
+        'x-user-id': user.email
+      }
+    })
+    return await parseFetchResponse(response)
+  } catch (err) {
+    console.error('Error occurred when trying to POST /customers')
+    console.error(err)
+  }
+}
